c7b61db18440ac1b8a59f215c5b5ac5eb7ed4411
chennisvkl <30309173+chennisvkl@users.noreply.github.com>
2017-08-11 17:20:17 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int main()
 {
-   int number;
-
-    printf("Enter a number: ");
-    scanf("%d", &number);
-
-    if (number <= 0)
+   int num;
+   printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num <= 0)
     {
-        if (number == 0)
+        if (num == 0)
             printf("You entered 0.");
         else
             printf("You entered a negative number.");
